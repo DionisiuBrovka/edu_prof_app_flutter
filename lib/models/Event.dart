bcb93690d06611ad84fba2a6a66e55db576355df
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2024-01-07 22:40:11 +0300

    make calendar selections

--- lib/models/Event.dart
+++ lib/models/Event.dart
@@ -1,3 +1,7 @@
+import 'package:edu_prof_app_flutter/api/ApiController.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class Events {
   int? id;
   String? eDate;
@@ -9,5 +13,23 @@ class Events {
     id = json['id'];
     eDate = json['e_date'];
     title = json['title'];
+  }
+
+  static Future<List<Events>> getAllObjectsList() async {
+    return fetchList(ApiController.getAPIUri('event/'));
+  }
+
+  static Future<List<Events>> fetchList(Uri uriForFetch) async {
+    final response = await http.get(uriForFetch);
+
+    if (response.statusCode == 200) {
+      final List body = json.decode(utf8.decode(response.bodyBytes));
+      final List<Events> dataFetched =
+          body.map((e) => Events.fromJson(e)).toList();
+
+      return dataFetched;
+    } else {
+      throw Exception('Failed to load Establishments list !!!');
+    }
   }
 }
--- lib/screens/EventsListPage.dart
+++ lib/screens/EventsListPage.dart
@@ -1,4 +1,5 @@
 import 'package:edu_prof_app_flutter/elements/NavBar.dart';
+import 'package:edu_prof_app_flutter/models/Event.dart';
 import 'package:edu_prof_app_flutter/templates/WideTemplate.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -19,14 +20,78 @@ class _EventsListPageState extends State<EventsListPage> {
         backgroundColor: Theme.of(context).primaryColor,
         foregroundColor: Theme.of(context).primaryColorLight,
       ),
-      body: WideTemplate(
-          body: TableCalendar(
-              locale: 'ru',
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              focusedDay: DateTime.now(),
-              availableCalendarFormats: const {CalendarFormat.month: 'Month'})),
+      body: FutureBuilder(
+          future: Events.getAllObjectsList(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return const Center(child: Text('Что то пошло не так ...'));
+            } else if (snapshot.hasData) {
+              return WideTemplate(
+                headFixed: CalendarView(
+                  events: snapshot.data!,
+                ),
+              );
+            } else {
+              return const Center(child: Text('Что то пошло не так ...'));
+            }
+          }),
       drawer: const NavBar(),
     );
+  }
+}
+
+class CalendarView extends StatefulWidget {
+  final List<Events> events;
+  const CalendarView({super.key, required this.events});
+
+  @override
+  State<CalendarView> createState() => _CalendarViewState();
+}
+
+class _CalendarViewState extends State<CalendarView> {
+  late DateTime? _selectedDay;
+  late DateTime _focusedDay;
+
+  @override
+  void initState() {
+    _selectedDay = DateTime.now();
+    _focusedDay = DateTime.now();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      locale: 'ru',
+      firstDay: DateTime.utc(2010, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: _focusedDay,
+      availableCalendarFormats: const {CalendarFormat.month: 'Month'},
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      eventLoader: _getEventsForDay,
+      onDaySelected: _onDateSelect,
+      selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+    );
+  }
+
+  List<Events> _getEventsForDay(DateTime day) {
+    final List<Events> _list = widget.events
+        .where((element) => isSameDay(DateTime.parse(element.eDate!), day))
+        .toList();
+
+    return _list;
+  }
+
+  void _onDateSelect(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      if (isSameDay(selectedDay, _selectedDay)) {
+        _selectedDay = null;
+      } else {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+      }
+    });
   }
 }
